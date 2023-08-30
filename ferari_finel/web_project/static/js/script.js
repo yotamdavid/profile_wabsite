0386ba0a90ec87753ecddae2ca09101a22731ccc
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-08-30 17:10:49 +0300

    Update script.js

--- ferari_finel/web_project/static/js/script.js
+++ ferari_finel/web_project/static/js/script.js
@@ -1,1 +1,9 @@
+function toggleNav() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
+
+function rotateCard(card) {
+    card.classList.toggle('flipped');
+}
 
