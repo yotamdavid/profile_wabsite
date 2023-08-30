a9854875b3c90c9928a80ec974456913d0875d57
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-08-30 16:28:49 +0300

    Update app.py

--- ferari_finel/web_project/app.py
+++ ferari_finel/web_project/app.py
@@ -6,17 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
-
-@app.route('/projects')
-def projects():
-    return render_template('projects.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
