243f5cd097e98eed8eaedf1dfbca85298f216b96
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-08-31 01:13:45 +0300

    Update script.js

--- web_project/static/js/script.js
+++ web_project/static/js/script.js
@@ -5,20 +5,14 @@ function toggleNav() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    const skillsCategory = document.getElementById('skills-category');
-    const educationCategory = document.getElementById('education-category');
-    const militaryCategory = document.getElementById('military-category');
+    const categories = document.querySelectorAll('.category');
 
-    skillsCategory.addEventListener('click', () => showCategoryInfo(skillsCategory));
-    educationCategory.addEventListener('click', () => showCategoryInfo(educationCategory));
-    militaryCategory.addEventListener('click', () => showCategoryInfo(militaryCategory));
+    categories.forEach(category => {
+        const categoryTitle = category.querySelector('.category-title');
 
-    function showCategoryInfo(category) {
-        const categories = document.querySelectorAll('.category');
-        categories.forEach(cat => {
-            cat.classList.remove('active');
-        });
-
-        category.classList.add('active');
-    }
-});
+        categoryTitle.addEventListener('click', () => {
+            categories.forEach(cat => {
+                if (cat !== category) {
+                    cat.classList.remove('active');
+                }
+            });
