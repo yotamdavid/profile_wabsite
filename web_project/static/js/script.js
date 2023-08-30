b243e56b4137a8be19305b1086c434fb2bcd7c15
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-08-31 00:39:55 +0300

    Update script.js

--- web_project/static/js/script.js
+++ web_project/static/js/script.js
@@ -3,3 +3,23 @@ function toggleNav() {
     var navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const skillsCategory = document.getElementById('skills-category');
+    const educationCategory = document.getElementById('education-category');
+    const militaryCategory = document.getElementById('military-category');
+
+    skillsCategory.addEventListener('click', () => showCategoryInfo(skillsCategory));
+    educationCategory.addEventListener('click', () => showCategoryInfo(educationCategory));
+    militaryCategory.addEventListener('click', () => showCategoryInfo(militaryCategory));
+
+    function showCategoryInfo(category) {
+        const categories = document.querySelectorAll('.category');
+        categories.forEach(cat => {
+            cat.classList.remove('active');
+        });
+
+        category.classList.add('active');
+    }
+});
+
