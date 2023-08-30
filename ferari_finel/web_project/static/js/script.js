6c959d07af940268cfd437d6e534e2223c9ff77c
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-08-30 17:21:51 +0300

    Update script.js

--- ferari_finel/web_project/static/js/script.js
+++ ferari_finel/web_project/static/js/script.js
@@ -1,9 +1,22 @@
+// Toggle mobile navigation
 function toggleNav() {
     var navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 }
 
+// Flip project card on hover
 function rotateCard(card) {
     card.classList.toggle('flipped');
-}
 
+    // Toggle the visibility of project descriptions
+    var projectDesc = card.querySelector('.project-description');
+    var projectDescFlipped = card.querySelector('.project-description-flipped');
+
+    if (card.classList.contains('flipped')) {
+        projectDesc.style.display = 'none';
+        projectDescFlipped.style.display = 'block';
+    } else {
+        projectDesc.style.display = 'block';
+        projectDescFlipped.style.display = 'none';
+    }
+}
