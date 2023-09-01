2e54fb85a053aee07582c4b400272ce57dc022c2
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-09-01 04:14:22 +0300

    Update script.js

--- web_project/static/js/script.js
+++ web_project/static/js/script.js
@@ -5,47 +5,23 @@ function toggleNav() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    const skillsCategory = document.getElementById('skills-category');
-    const educationCategory = document.getElementById('education-category');
-    const militaryCategory = document.getElementById('military-category');
-
-    skillsCategory.addEventListener('click', () => showCategoryInfo(skillsCategory));
-    educationCategory.addEventListener('click', () => showCategoryInfo(educationCategory));
-    militaryCategory.addEventListener('click', () => showCategoryInfo(militaryCategory));
-
-    function showCategoryInfo(category) {
-        const categories = document.querySelectorAll('.category');
-        categories.forEach(cat => {
-            cat.classList.remove('active');
-        });
-
-        category.classList.add('active');
-    }
-}); 
-
-let lastClickedCategory = null;
-
-function showCategoryInfo(category) {
     const categories = document.querySelectorAll('.category');
-    categories.forEach(cat => {
-        cat.classList.remove('active');
+    let lastClickedCategory = null;
+
+    categories.forEach(category => {
+        category.addEventListener('click', () => toggleCategory(category));
     });
 
-    if (lastClickedCategory !== category) {
-        category.classList.add('active');
+    function toggleCategory(category) {
+        if (lastClickedCategory === category) {
+            category.classList.toggle('active');
+        } else {
+            categories.forEach(cat => {
+                cat.classList.remove('active');
+            });
+            category.classList.add('active');
+        }
+
         lastClickedCategory = category;
-    } else {
-        lastClickedCategory = null;
     }
-
-    updateOrangeStripePosition();
-}
-
-function updateOrangeStripePosition() {
-    const activeCategory = document.querySelector('.category.active');
-    const orangeStripe = activeCategory.querySelector('.category-orange-stripe');
-    if (orangeStripe) {
-        orangeStripe.style.transform = activeCategory ? 'translateY(0)' : 'translateY(100%)';
-    }
-}
-
+});
