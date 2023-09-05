72b2f17907b146bef30339c480cfe6b6c5c46163
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-09-05 17:52:31 +0300

    Update setup.sh

--- web_project/setup.sh
+++ web_project/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash -xe
-sudo yum install python -y
-sudo yum install python-pip -y
-sudo pip install ansible
+sudo -S yum install python -y
+sudo -S yum install python-pip -y
+sudo -S pip install ansible
 ansible-playbook -i inventory.ini my_playbook.yml
