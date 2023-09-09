dac20a3c80bbee80f0f71997aed43c953451628b
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-09-09 16:47:48 +0300

    Update AWS_Env.tf

--- AWS_Env.tf
+++ AWS_Env.tf
@@ -102,10 +102,9 @@ resource "aws_lb" "my_load_balancer" {
   subnets            = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
   security_groups    = [aws_security_group.my_security_group.id]
   load_balancer_type = "application"
-  enable_deletion_protection = false
 
-  enable_http2               = true
   enable_deletion_protection = false
+  enable_http2               = true
   idle_timeout               = 60
 
   tags = {
