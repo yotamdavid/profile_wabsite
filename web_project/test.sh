64c73c57152c94db9c2422f9214b6658e3b06841
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-09-10 11:54:04 +0300

    Update test.sh

--- web_project/test.sh
+++ web_project/test.sh
@@ -3,7 +3,7 @@ set -ex
 
 sleep 20
 # test1
-timeout 20 curl -s http://52.87.200.27:5000
+timeout 20 curl -s http://54.209.148.229:5000
 
 # <-- שורה מעודכנת -->
 echo "Test finished successfully"  # פלט מובנה לפלט של Jenkins
