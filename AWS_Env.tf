39b2eeb842aa66fc85f0908c9630c6ab8da0526d
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-09-10 11:46:26 +0300

    Update AWS_Env.tf

--- AWS_Env.tf
+++ AWS_Env.tf
@@ -1,32 +1,33 @@
+# הגדרת VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block          = "10.0.0.0/16"
   enable_dns_support  = true
   enable_dns_hostnames = true
 }
 
-resource "aws_subnet" "subnet_a" {
-  vpc_id           = aws_vpc.my_vpc.id
-  cidr_block       = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
+# הגדרת Internet Gateway
+resource "aws_internet_gateway" "my_internet_gateway" {}
+
+# קישור ה-Internet Gateway ל-VPC
+resource "aws_vpc_attachment" "my_vpc_attachment" {
+  vpc_id             = aws_vpc.my_vpc.id
+  internet_gateway_id = aws_internet_gateway.my_internet_gateway.id
 }
 
-resource "aws_subnet" "subnet_b" {
-  vpc_id           = aws_vpc.my_vpc.id
-  cidr_block       = "10.0.1.0/24"
-  availability_zone = "us-east-1b"
-}
-
+# הגדרת Security Group
 resource "aws_security_group" "my_security_group" {
-  name = "MySecurityGroup"
+  name = "SecurityGroupForMyApplication"
   vpc_id = aws_vpc.my_vpc.id
 
+  # Inbound rules
   ingress {
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # כדאי להתאים את זה לצרכים שלך
   }
 
+  # Outbound rules (allow all traffic)
   egress {
     from_port   = 0
     to_port     = 0
@@ -35,20 +36,45 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 
+# הגדרת Subnets
+resource "aws_subnet" "subnet_a" {
+  vpc_id           = aws_vpc.my_vpc.id
+  cidr_block       = "10.0.0.0/24"
+  availability_zone = "us-east-1a"  # שנה את האזור כרצונך
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id           = aws_vpc.my_vpc.id
+  cidr_block       = "10.0.1.0/24"
+  availability_zone = "us-east-1b"  # שנה את האזור כרצונך
+}
+
+# הגדרת Route Table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# הגדרת SubnetRouteTableAssociation
+resource "aws_main_route_table_association" "subnet_a_association" {
+  vpc_id      = aws_vpc.my_vpc.id
+}
+
+# הגדרת EC2 Instances
 resource "aws_instance" "ec2_instance_a" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI ID (us-east-1)
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.subnet_a.id
   security_groups = [aws_security_group.my_security_group.id]
 }
 
 resource "aws_instance" "ec2_instance_b" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI ID (us-east-1)
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.subnet_b.id
   security_groups = [aws_security_group.my_security_group.id]
 }
 
+# הגדרת Target Group
 resource "aws_lb_target_group" "my_target_group" {
   name     = "MyTargetGroup"
   port     = 5000
@@ -62,6 +88,7 @@ resource "aws_lb_target_group" "my_target_group" {
   }
 }
 
+# הגדרת Load Balancer
 resource "aws_lb" "my_load_balancer" {
   name               = "MyLoadBalancer"
   subnets            = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
@@ -77,22 +104,7 @@ resource "aws_lb" "my_load_balancer" {
   }
 }
 
-resource "null_resource" "create_ami" {
-  triggers = {
-    instance_ids = "${aws_instance.ec2_instance_a.id}${aws_instance.ec2_instance_b.id}"
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-      #!/bin/bash
-      instance_ids=(${aws_instance.ec2_instance_a.id} ${aws_instance.ec2_instance_b.id})
-      for instance_id in "\${instance_ids[@]}"; do
-        aws ec2 create-image --instance-id "\$instance_id" --name "MyServer-Image-\$instance_id" --description "AMI for MyServer" --no-reboot
-      done
-    EOT
-  }
-}
-
+# הגדרת Listener
 resource "aws_lb_listener" "my_listener" {
   load_balancer_arn = aws_lb.my_load_balancer.arn
   port              = 80
